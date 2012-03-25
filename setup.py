c98ad1a9de7f15cb62a706b3a29e1acb27603b58
Simone Orsi <simahawk@gmail.com>
2012-03-25 16:45:07 +0200

    removed paster stuff

--- setup.py
+++ setup.py
@@ -34,6 +34,4 @@ setup(name='collective.archiveviewer',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
